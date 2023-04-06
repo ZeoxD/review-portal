8c57ca3c02855749a0fa6380d4bb49a9f88e09b7
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-04-06 16:40:25 +0530

    Added Navbar functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,22 +5,25 @@ const login = (props) => {
         <Container>
             <Nav>
                 <a href="/">
-                    <img src="/images/preview-logo.jpg" alt="" />
+                    <img src="/images/preview-logo-large.jpg" alt="" height="35px" width="135px"/>
                 </a>
+                <div>
+                    <Join>Sign Up</Join>
+                </div>
             </Nav>
         </Container>
     )
 }
 
 const Container = styled.div`
-    padding: 0px;
+padding: 0px;
 `;
+
+
 const Nav = styled.nav`
-    max-width: 1128px;
+    max-width: 98vw;
     margin: auto;
-    padding: 12px 0 16px;<div>
-    <h1>Login</h1>
-</div>
+    padding: 12px 0 16px;
     display: flex;
     align-items:center;
     position: relative;
@@ -30,9 +33,26 @@ const Nav = styled.nav`
     & > a {
         width: 135px;
         height: 34px;
+        box-shadow: 1px 1px 3px black;
         @media (max-width: 768px) {
             padding: 0 5px;
         }
+    }
+`;
+
+const Join = styled.a`
+    font-size: 16px;
+    padding: 10px 12px;
+    text-decoration: none;
+    color: rgba(0, 0, 0, 0.6);
+    margin-right: 12px;
+    border-radius: 5px;
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.08);
+        color: rgba(0, 0, 0, 0.9);
+        text-decoration: none;
+
     }
 `;
 
