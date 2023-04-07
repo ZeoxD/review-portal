ce6ddb54b476ee70a19e9c0755dc09dd2e68f587
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-04-08 00:42:55 +0530

    login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { connect } from 'react-redux';
+import { signInAPI } from "../actions";
 
 const Login = (props) => {
     return (
@@ -19,7 +20,7 @@ const Login = (props) => {
                     <img src="/images/preview-login-small.jpg" alt="" />
                 </Hero>
                 <Form>
-                    <Google>
+                    <Google onClick={() => props.signIn()}>
                         <img src="/images/google-logo.png" alt="" height="26px" width="auto"></img>
                         Sign in with Google
                     </Google>
@@ -181,8 +182,8 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
+    signIn: () => dispatch(signInAPI()),
 
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "./firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA2WCbJXg3A8AW4j2ZopcP1292-pioY8M0",
+    authDomain: "preview-system.firebaseapp.com",
+    projectId: "preview-system",
+    storageBucket: "preview-system.appspot.com",
+    messagingSenderId: "706225416000",
+    appId: "1:706225416000:web:ae52e45afbddffb03fbca5"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+
+export default db;
