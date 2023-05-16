518a5ae22ed2a4231c214f2de2fe09ee5e3b41b0
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-05-16 23:36:54 +0530

    added auth context to index file

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from "react-redux";
-import store from "./store";
+import { AuthContextProvider } from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store= {store} >
+    <AuthContextProvider>
       <App />
-    </Provider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
