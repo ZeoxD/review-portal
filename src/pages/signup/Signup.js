7650d6087bb52e5e0b2a009f4e6d863a3e08ba14
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-05-22 13:07:01 +0530

    added signup component and index css styles

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -4,9 +4,31 @@ import { useState } from 'react'
 import './Signup.css'
 
 function Signup() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDisplayName] = useState('') 
+    const [thumbnail, setThumbnail] = useState(null)
+
     return (
         <form className="auth-form">
-            <h2>Signup</h2>
+            <h1>Sign up</h1>
+            <label>
+                <span>Email: </span>
+                <input required type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+            </label>
+            <label>
+                <span>Password: </span>
+                <input required type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+            </label>
+            <label>
+                <span>Display Name: </span>
+                <input required type="text" onChange={(e) => setDisplayName(e.target.value)} value={displayName} />
+            </label>
+            <label>
+                <span>Profile Thumbail: </span>
+                <input required type="file" />
+            </label>
+            <button className="btn">Sign up</button>
         </form>
     )
 }
