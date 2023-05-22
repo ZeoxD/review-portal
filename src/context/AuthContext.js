2691cf377c17195b0721158d41712a32d3472c41
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-05-22 20:51:32 +0530

    updated useSignup hook

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useReducer, useEffect } from 'react'
-import { projectAuth } from '../firebase/firebase'
+import { projectAuth } from '../firebase/config'
 
 export const AuthContext = createContext()
 
