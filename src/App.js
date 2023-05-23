5a641bd84cf0c56309102385e5dc9b7b5348fc74
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-05-23 18:18:23 +0530

    added route guards & redirects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import {useAuthContext} from './hooks/useAuthContext'
+
+// styles & images
 import './App.css';
+
+//pages & components
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup"
 import Home from "./pages/home/Home"
@@ -9,21 +14,25 @@ import Explore from "./pages/explore/Explore";
 import Preview from "./pages/preview/Preview";
 
 function App() {
+  const { user, authIsReady } = useAuthContext()
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route path="/login" element = {<Login/>} />
-            <Route path="/signup" element = {<Signup/>} />
-            <Route path="/home" element = {<Home/>} />
-            <Route path="/explore" element = {<Explore/>} />
-            <Route path="/preview" element = {<Preview/>} />
-            <Route exact path="/" element = {<Dashboard/>} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      { authIsReady &&
+        <BrowserRouter>
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route path="/login" element = { !user ? <Login/> : <Navigate to="/"/> } />
+              <Route path="/signup" element = { !user ? <Signup/> : <Navigate to="/"/> } />
+              <Route path="/home" element = { user ? <Home/> : <Navigate to="/preview"/> } />
+              <Route path="/explore" element = { user ? <Explore/> : <Navigate to="/preview"/> } />
+              <Route path="/preview" element = { !user ? <Preview/> : <Navigate to="/"/> } />
+              <Route exact path="/" element = { user ? <Dashboard/> : <Navigate to="/preview"/> } />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      }
     </div>
   );
 }
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -26,7 +26,7 @@ function Login() {
                 <input required type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
             </label>
             {!isPending && <button className="btn">Login</button>}
-            {isPending && <button className="btn" disabled>Login</button>}
+            {isPending && <button className="btn" disabled>loading...</button>}
             {error && <div className="error">{error}</div>}
         </form>
     )
